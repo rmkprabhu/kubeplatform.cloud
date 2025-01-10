b40f2ff3f859c485b54b92367886ac26e4b5bbce
Manikanda Prabhu RaviChandran <rmkp@hotmail.com>
2025-01-10 20:10:22 +0530

    added new kubenetes questions

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/sd
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -19,15 +18,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'kubernetes-data': ['./src/data/kubernetes.ts']
-        }
-      }
-    }
-  }
 }));
 
 //added stable static web app for hosting
